e7f9a7502467342a6f07a9a290fafd83cbde28bd
niff <ju.bogucka@gmail.com>
2015-03-19 20:58:49 +0100

    repair migrations

--- IglaClub.Web/Migrations/Configuration.cs
+++ IglaClub.Web/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace IglaClub.Web.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Models.IglaClubDbContext context)
