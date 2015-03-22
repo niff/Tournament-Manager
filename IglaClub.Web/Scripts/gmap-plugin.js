78ece5cf920971cefe0a59e0910b0a669a3c5735
niff <ju.bogucka@gmail.com>
2015-03-22 22:26:29 +0100

    maps - setting zoom and location

--- IglaClub.Web/Scripts/gmap-plugin.js
+++ IglaClub.Web/Scripts/gmap-plugin.js
@@ -15,6 +15,7 @@
                 }
             };
             
+            
             var map = new google.maps.Map(document.getElementById(mapCanvasId), mapOptions);
             //var defaultBounds = new google.maps.LatLngBounds(
             //    new google.maps.LatLng(-33.8902, 151.1759),
@@ -22,13 +23,62 @@
             //map.fitBounds(defaultBounds);
             //map.setZoom(16);
             
-            var marker = new google.maps.Marker({
-                map: map,
-                title: 'Club\'s location',
-                position: position
+            //var marker = new google.maps.Marker({
+            //    map: map,
+            //    title: 'Club\'s location',
+            //    position: position
+            //});
+
+            //markers.push(marker);
+            
+            var input = (document.getElementById('pac-input'));
+
+            var searchBox = new google.maps.places.SearchBox((input));
+
+            google.maps.event.addListener(searchBox, 'places_changed', function () {
+                var places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+                for (var i = 0, marker; marker = markers[i]; i++) {
+                    marker.setMap(null);
+                }
+
+                markers = [];
+                var bounds = new google.maps.LatLngBounds();
+                for (var i = 0, place; place = places[i]; i++) {
+                    var image = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                    };
+
+                    // Create a marker for each place.
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        icon: image,
+                        title: place.name,
+                        position: place.geometry.location
+                    });
+
+                    utils.setHiddenCoordinates(place.geometry.location);
+
+                    markers.push(marker);
+
+                    bounds.extend(place.geometry.location);
+                }
+
+                map.fitBounds(bounds);
             });
 
-            markers.push(marker);
+            google.maps.event.addListener(map, 'bounds_changed', function () {
+                var bounds = map.getBounds();
+                searchBox.setBounds(bounds);
+                map.setZoom(14);
+            });
             
         };
         google.maps.event.addDomListener(window, 'load', initMap);
@@ -61,5 +111,8 @@
         //    };
         //    return image;
         //}
+        setHiddenCoordinates : function(location) {
+            $("#coords").val(location);
+        }
     };
 })(jQuery);
