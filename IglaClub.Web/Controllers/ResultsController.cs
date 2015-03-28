e98b15979e1f694bb3279e7ca496e199e20f3e27
niff <ju.bogucka@gmail.com>
2015-03-28 22:50:29 +0100

    list changes

--- IglaClub.Web/Controllers/ResultsController.cs
+++ IglaClub.Web/Controllers/ResultsController.cs
@@ -215,7 +215,8 @@ namespace IglaClub.Web.Controllers
                 {
                     TournamentScoringType = tournament.TournamentScoringType,
                     PairsInTounament = pairRepository.GetPairsByTournament(tournamentId).OrderByDescending(p=>p.Score).ToList(),
-                    PairNumberMaxPoints = pairNumberMaxPoints
+                    PairNumberMaxPoints = pairNumberMaxPoints,
+                    TournamentId = tournamentId
                     
                 };
 
