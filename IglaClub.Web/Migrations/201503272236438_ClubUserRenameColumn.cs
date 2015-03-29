121cd92148bacd6a226dedd2bfacf45207e71003
igiel <bartlomiej.igla@gmail.com>
2015-03-29 22:39:01 +0200

    Add migrations columnin club user

--- IglaClub.Web/Migrations/201503272236438_ClubUserRenameColumn.cs
+++ IglaClub.Web/Migrations/201503272236438_ClubUserRenameColumn.cs
@@ -7,8 +7,8 @@ namespace IglaClub.Web.Migrations
     {
         public override void Up()
         {
-            RenameColumn(table: "dbo.ClubUsers", name: "Users_Id", newName: "User_Id");
-            RenameColumn(table: "dbo.ClubUsers", name: "Clubs_Id", newName: "Club_Id");
+            //RenameColumn(table: "dbo.ClubUsers", name: "Users_Id", newName: "User_Id");
+            //RenameColumn(table: "dbo.ClubUsers", name: "Clubs_Id", newName: "Club_Id");
             //RenameIndex(table: "dbo.ClubUsers", name: "IX_Clubs_Id", newName: "IX_Club_Id");
             //RenameIndex(table: "dbo.ClubUsers", name: "IX_Users_Id", newName: "IX_User_Id");
             //RenameIndex(table: "dbo.Club", name: "IX_Users_Id", newName: "IX_User_Id");
@@ -16,10 +16,10 @@ namespace IglaClub.Web.Migrations
         
         public override void Down()
         {
-            RenameIndex(table: "dbo.ClubUsers", name: "IX_User_Id", newName: "IX_Users_Id");
-            RenameIndex(table: "dbo.ClubUsers", name: "IX_Club_Id", newName: "IX_Clubs_Id");
-            RenameColumn(table: "dbo.ClubUsers", name: "Club_Id", newName: "Clubs_Id");
-            RenameColumn(table: "dbo.ClubUsers", name: "User_Id", newName: "Users_Id");
+            //RenameIndex(table: "dbo.ClubUsers", name: "IX_User_Id", newName: "IX_Users_Id");
+            //RenameIndex(table: "dbo.ClubUsers", name: "IX_Club_Id", newName: "IX_Clubs_Id");
+            //RenameColumn(table: "dbo.ClubUsers", name: "Club_Id", newName: "Clubs_Id");
+            //RenameColumn(table: "dbo.ClubUsers", name: "User_Id", newName: "Users_Id");
         }
     }
 }
