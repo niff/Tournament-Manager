39ac37a4315fadd3121bb7c7fae574561e5db3fd
niff <ju.bogucka@gmail.com>
2015-03-30 23:08:56 +0200

    manage view changes

--- IglaClub.Web/App_Start/BundleConfig.cs
+++ IglaClub.Web/App_Start/BundleConfig.cs
@@ -12,15 +12,17 @@ namespace IglaClub.Web
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-{version}.js"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/qtip").Include(
+                        "~/Content/qtip/jquery.qtip.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/DateTimepickerJs").Include
                 ("~/Scripts/jquery.datetimepicker.js"));
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -32,7 +34,8 @@ namespace IglaClub.Web
                         "~/Content/bootstrap/css/bootstrap-responsive.css",
                         "~/Content/global.css",
                         "~/Content/dataTables/css/jquery.dataTables.css",
-                        "~/Content/dataTables/css/jquery.dataTables_themeroller.css"
+                        "~/Content/dataTables/css/jquery.dataTables_themeroller.css",
+                        "~/Content/qtip/jquery.qtip.css"
                         ));
             
             bundles.Add(new ScriptBundle("~/bundles/dataTables").Include(
@@ -44,20 +47,7 @@ namespace IglaClub.Web
 
             bundles.Add(new StyleBundle("~/Content/jqueryui").Include
                 ("~/Content/jquery-ui.css"));
-        
-            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-            //            "~/Content/themes/base/jquery.ui.core.css",
-            //            "~/Content/themes/base/jquery.ui.resizable.css",
-            //            "~/Content/themes/base/jquery.ui.selectable.css",
-            //            "~/Content/themes/base/jquery.ui.accordion.css",
-            //            "~/Content/themes/base/jquery.ui.autocomplete.css",
-            //            "~/Content/themes/base/jquery.ui.button.css",
-            //            "~/Content/themes/base/jquery.ui.dialog.css",
-            //            "~/Content/themes/base/jquery.ui.slider.css",
-            //            "~/Content/themes/base/jquery.ui.tabs.css",
-            //            "~/Content/themes/base/jquery.ui.datepicker.css",
-            //            "~/Content/themes/base/jquery.ui.progressbar.css",
-            //            "~/Content/themes/base/jquery.ui.theme.css"));
+
         }
     }
 }
