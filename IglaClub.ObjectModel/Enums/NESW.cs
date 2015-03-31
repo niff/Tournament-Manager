f43774e630b413cf9969eaa3ca456495db74d5c8
niff <ju.bogucka@gmail.com>
2015-03-31 21:06:37 +0200

    result table column changes

--- IglaClub.ObjectModel/Enums/NESW.cs
+++ IglaClub.ObjectModel/Enums/NESW.cs
@@ -1,16 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IglaClub.ObjectModel.Enums
 {
     [Flags]
     public enum NESW
     {
+        [Display(Name = "n/a")]
         Unavailable = 1,
+        [Display(Name = "N")]
         North = 2,
+        [Display(Name = "E")]
         East = 4,
+        [Display(Name = "S")]
         South = 8,
+        [Display(Name = "W")]
         West = 16,
+        [Display(Name = "p/o")]
         PassedOut = 32,
+        [Display(Name = "d/s")]
         DirectorScore = 64
     }
 }
