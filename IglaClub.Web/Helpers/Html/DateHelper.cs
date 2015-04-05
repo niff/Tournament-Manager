95caa973d695b920868d090bb973115a65c41f71
niff <ju.bogucka@gmail.com>
2015-04-06 00:14:23 +0200

    changes to date helper

--- IglaClub.Web/Helpers/Html/DateHelper.cs
+++ IglaClub.Web/Helpers/Html/DateHelper.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Web;
 
 namespace IglaClub.Web.Helpers.Html
 {
     public static class DateHelper
     {
-        public static string DaysLeftToDate(DateTime inputDate)
+        public static IHtmlString DaysLeftToDate(DateTime? inputDate)
         {
+            if(inputDate == null) return new HtmlString("not set yet");
+            DateTime date = (DateTime) inputDate;;
+            var fullDate = date.ToLongDateString();
             const int weekDays = 7;
-            int span = (inputDate.Date - DateTime.Today).Days;
+            int span = (date.Date - DateTime.Today).Days;
             if (span <= weekDays && span >= -weekDays)
             {
                 if (span == 0)
                 {
-                    return "today";
+                    return new HtmlString(String.Format("<span title=\"{0}\">today</span>", fullDate));
                 }
 
                 string dateIsFuture = "";
@@ -27,12 +31,11 @@ namespace IglaClub.Web.Helpers.Html
                 }
                 var daysCount = Math.Abs(span);
                 var daysPlural = daysCount != 1 ? "s" : String.Empty;
-                
 
-                return String.Format("{3}{0} day{1}{2}", 
-                    daysCount, daysPlural, dateIsPast, dateIsFuture);
+                return new HtmlString(String.Format("<span title=\"{4}\">{3}{0} day{1}{2}</span>",
+                    daysCount, daysPlural, dateIsPast, dateIsFuture, fullDate));
             }
-            return inputDate.ToLongDateString();
+            return new HtmlString(String.Format("<span>{0}</span>", fullDate));
         }
     }
 }
