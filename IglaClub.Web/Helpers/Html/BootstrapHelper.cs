76bb3acd43e5ddf5a69f8d6b4b3f16a218d06a00
niff <ju.bogucka@gmail.com>
2015-04-10 14:00:40 +0200

    tourn. list refactoring

--- IglaClub.Web/Helpers/Html/BootstrapHelper.cs
+++ IglaClub.Web/Helpers/Html/BootstrapHelper.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 
 namespace IglaClub.Web.Helpers.Html
 {
     public static class BootstrapHelper
     {
-
         public static IHtmlString Popover(string content, string icon)
         {
             var popover = String.Format("<a role=\"button\" data-toggle=\"popover\"  class=\"popovers\" tabindex=\"0\"   data-content=\"{0}\" ><i class=\"glyphicon glyphicon-{1}\"></i></a>", content, icon);
