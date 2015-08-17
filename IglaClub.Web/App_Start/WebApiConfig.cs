e18e7a27678eae92b49de6b59dc52cad87686ace
niff <ju.bogucka@gmail.com>
2015-08-17 22:38:09 +0200

    .

--- IglaClub.Web/App_Start/WebApiConfig.cs
+++ IglaClub.Web/App_Start/WebApiConfig.cs
@@ -10,6 +10,11 @@ namespace IglaClub.Web
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
+                name: "DefaultApi1",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: null
+            );
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
